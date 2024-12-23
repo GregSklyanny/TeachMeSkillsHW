ed135d5a1f0eba8d6ba4b918b5ff1f03ac565f83
GregSklyanny <gregsklyanny@gmail.com>
2024-12-23 19:51:16 +0300

    Initial commit

---
+++ HW1Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using HW1Library;
+
+
+namespace HW1Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Utils utils = new HW1Library.Utils();
+            while (true)
+            {
+                Console.WriteLine("Введите целое число");
+                string input = Console.ReadLine();
+                var s = utils.OddOrEvenAnalyzer(input);
+                Console.WriteLine($"Ответ: {s.ToString()}");
+            }
+        }
+    }
+}
---
+++ HW1Library/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1Library
+{
+    public class Utils
+    {
+        public string OddOrEvenAnalyzer(string str)
+        {
+            int number;
+            if (int.TryParse(str, out number))
+            {
+                return number % 2 == 0 ? $"Число {number} четное" : $"Число {number} нечетное";
+            }
+
+            return "Это не целое число!";
+        }
+    }
+}
