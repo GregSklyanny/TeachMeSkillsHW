05c51b099e254d0ebbc562d4789cc000cdc464e1
GregSklyanny <gregsklyanny@gmail.com>
2024-12-26 22:26:50 +0300

    Console App

--- HW4/2DArrays/Program.cs
+++ HW4/2DArrays/Program.cs
@@ -1,10 +1,108 @@
-﻿namespace _2DArrays
+﻿using System.Text;
+
+namespace _2DArrays
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int m, n;
+            bool flag = true;
+
+            while (flag)
+            {
+                try
+                {
+                    Console.Clear();
+                    Console.WriteLine("Введите размерность:");
+                    Console.Write("Количество строк = ");
+                    if (!int.TryParse(Console.ReadLine(), out m)) throw new ArgumentException("неверно введено значение");
+                    Console.Write("Количество столбцов = ");
+                    if (!int.TryParse(Console.ReadLine(), out n)) throw new ArgumentException("неверно введено значение");
+
+                    var array = new int[m, n];
+                    Console.WriteLine("Введите значения:");
+                    for (int x = 0; x < m; x++)
+                    {
+                        Console.WriteLine($"{x+1} строка");
+                        for (int y = 0; y < n; y++)
+                        {
+                            if (!int.TryParse(Console.ReadLine(), out array[x, y])) throw new ArgumentException("неверно введено значение");
+                            
+                        }
+                        
+                    }
+                    Console.WriteLine("Итоговая матрица");
+                    Console.WriteLine(Utils.TransformArrayToString(array));
+
+                    bool flag2 = true;
+                    string str = "Выберите действие: \n q - выйти \n rev - реверс строки массива \n +- - количество положительных и отриательных элементов" +
+                        "\n sortAsc - сортировка по возрастанию \n sortDesc - сортировка по убыванию";
+                    while (flag2)
+                    {
+                        Console.WriteLine(str);
+                        string action = Console.ReadLine();
+                        
+                        switch(action)
+                        {
+                            case "q":
+                                flag2 = false;
+                                break;
+                            case "rev":
+                                Console.WriteLine("реверс какой строки?");
+                                int param = int.Parse(Console.ReadLine());
+                                Console.Clear();
+                                Utils.ReverseLine(array, param);
+                                Console.WriteLine(Utils.TransformArrayToString(array));
+                                break;
+                            case "sortAsc":
+                                Console.WriteLine("реверс какой строки?");
+                                param = int.Parse(Console.ReadLine());
+                                Console.Clear();
+                                Utils.SortAscending(array, param);
+                                Console.WriteLine(Utils.TransformArrayToString(array));
+                                break;
+                            case "sortDesc":
+                                Console.WriteLine("реверс какой строки?");
+                                param = int.Parse(Console.ReadLine());
+                                Console.Clear();
+                                Utils.SortDescending(array, param);
+                                Console.WriteLine(Utils.TransformArrayToString(array));
+                                break;
+                            case "+-":
+                                Console.WriteLine(Utils.NegativeAndPositiveCount(array));
+                                break;
+                            default:
+                                throw new ArgumentException("такой команды не существует");
+                        }
+                        
+                    }
+                    Console.Clear();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.Clear();
+                    Console.WriteLine(ex.Message);
+                    flag = IsNeedRestart();
+                    
+                }
+            }
+        }
+        public static bool IsNeedRestart()
+        {
+            string str = string.Empty;
+            ConsoleKey consoleKey;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Начать заново? y - начать заново, n - выйти из программы");
+            Console.ForegroundColor = ConsoleColor.White;
+            while (true)
+            {
+                consoleKey = Console.ReadKey().Key;
+                if (consoleKey == ConsoleKey.Y) return true;
+                if (consoleKey == ConsoleKey.N) return false;
+            }
+            return false;
         }
     }
 }
---
+++ HW4/2DArrays/Utils.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DArrays
+{
+
+    public class Utils
+    {
+        public static string TransformArrayToString(int[,] array)
+        {
+            StringBuilder sb = new StringBuilder();
+            int m = array.GetLength(0);
+            int n = array.GetLength(1);
+            //выводим массив
+            for (int x = 0; x < m; x++)
+            {
+                for (int y = 0; y < n; y++)
+                {
+                    sb.Append(array[x, y]);
+                    sb.Append(' ');
+                }
+                sb.Append('\n');
+
+            }
+            return sb.ToString();
+        }
+
+        public static int[,] ReverseLine(int[,] array, int i)
+        {
+            var list = new List<int>();
+            int dim = array.GetLength(1);
+            if (dim - 1 < i || i < 0) throw new ArgumentException("Неверный индекс строки");
+            for (int k = 0; k < dim; k++)
+            {
+                list.Add(array[i, k]);
+            }
+            list.Reverse();
+            for (int k = 0; k < dim; k++)
+            {
+                array[i,k] = list[k];
+            }
+            return array;
+        }
+        public static int[,] SortAscending(int[,] array, int i)
+        {
+            var list = new List<int>();
+            int dim = array.GetLength(1);
+            if (dim - 1 < i || i < 0) throw new ArgumentException("Неверный индекс строки");
+            for (int k = 0; k < dim; k++)
+            {
+                list.Add(array[i, k]);
+            }
+            list.Sort((a,b) => a - b);
+            for (int k = 0; k < dim; k++)
+            {
+                array[i, k] = list[k];
+            }
+            return array;
+        }
+        public static int[,] SortDescending(int[,] array, int i)
+        {
+            var list = new List<int>();
+            int dim = array.GetLength(1);
+            if (dim - 1 < i || i < 0) throw new ArgumentException("Неверный индекс строки");
+            for (int k = 0; k < dim; k++)
+            {
+                list.Add(array[i, k]);
+            }
+            list.Sort((a, b) => b - a);
+            for (int k = 0; k < dim; k++)
+            {
+                array[i, k] = list[k];
+            }
+            return array;
+        }
+        public static string NegativeAndPositiveCount(int[,] array)
+        {
+            int m = array.GetLength(0);
+            int n = array.GetLength(1);
+            int negativeCount = 0 , positiveCount = 0;
+            //выводим массив
+            for (int x = 0; x < m; x++)
+            {
+                for (int y = 0; y < n; y++)
+                {
+                    if (array[x,y] >= 0)
+                    {
+                        positiveCount++;
+                    }
+                    else
+                    {
+                        negativeCount++;
+                    }
+                }
+            }
+            return $"Положительных чисел = {positiveCount}, отрицательных чисел = {negativeCount}";
+        }
+    }
+}
