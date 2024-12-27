3b213f3f426836d5723afecbcf9d420347a12f00
GregSklyanny <gregsklyanny@gmail.com>
2024-12-27 15:23:39 +0300

    update

--- HW5/ConsoleStringMethods/Program.cs
+++ HW5/ConsoleStringMethods/Program.cs
@@ -4,7 +4,56 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("String Analyzer");
+            bool flag = true;
+
+            while (flag)
+            {
+                try
+                {
+                    Console.Clear();
+                    Console.WriteLine("Введите текст");
+                    string text = Console.ReadLine();
+
+                    bool flag2 = true;
+                    string str = "Выберите действие: \n q - выйти \n rev - реверс строки массива \n +- - количество положительных и отриательных элементов" +
+                        "\n sortAsc - сортировка по возрастанию \n sortDesc - сортировка по убыванию";
+                    while (flag2)
+                    {
+                        Console.WriteLine(str);
+                        string action = Console.ReadLine();
+                        switch(action)
+                        {
+                            case "q":
+                                flag2 = false;
+                                break;
+                            default:
+                                throw new ArgumentException("такой команды не существует");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Clear();
+                    Console.WriteLine(ex.Message);
+                    flag = IsNeedRestart();
+                }
+
+            }
+        }
+        public static bool IsNeedRestart()
+        {
+            string str = string.Empty;
+            ConsoleKey consoleKey;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Начать заново? y - начать заново, n - выйти из программы");
+            Console.ForegroundColor = ConsoleColor.White;
+            while (true)
+            {
+                consoleKey = Console.ReadKey().Key;
+                if (consoleKey == ConsoleKey.Y) return true;
+                if (consoleKey == ConsoleKey.N) return false;
+            }
+            return false;
         }
     }
 }
---
+++ HW5/ConsoleStringMethods/Utils.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleStringMethods
+{
+    public class Utils
+    {
+        public static string CheckMaxDigits(string[] input)
+        {
+            string result = "";
+            return result;
+        }
+        public static string CheckLongWordAndAppearance(string[] input)
+        {
+            string result = "";
+            return result;
+        }
+        public static string SwapDigitsToWords(string[] input)
+        {
+            string result = "";
+            return result;
+        }
+        public static string AffirmativeAndQuestions(string[] input)
+        {
+            string result = "";
+            return result;
+        }
+        public static string NotContainsCommasSentences(string[] input)
+        {
+            string result = "";
+            return result;
+        }
+        public static string BeginAndEndsWithTheSameLetter(string[] input)
+        {
+            string result = "";
+            return result;
+        }
+    }
+}
