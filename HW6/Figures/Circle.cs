f298c7dc7a5eef229ce1ededd8a1a3da7e677681
GregSklyanny <gregsklyanny@gmail.com>
2025-01-07 18:10:30 +0300

    HW6

---
+++ HW6/Figures/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    public class Circle : IShape, IShapeInfo
+    {
+        private double _radius;
+        public double Radius {
+            get {
+                return _radius; 
+            } 
+            private set {
+
+                if (value < 0) throw new ArgumentException("Радиус не может быть отрицательным");
+                _radius = value; 
+            } 
+        }
+        public string Name { get; private set; }
+        public Circle(string name, double radius)
+        {
+            Radius = radius;
+            Name = name;
+        }
+        public double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+}
---
+++ HW6/Figures/Square.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    public class Square: IShape, IShapeInfo
+    {
+        private double _side;
+        public double Side
+        {
+            get
+            {
+                return _side;
+            }
+            private set
+            {
+
+                if (value < 0) throw new ArgumentException("Сторона не может быть отрицательной длины");
+                _side = value;
+            }
+        }
+        public string Name { get; private set; }
+        public Square(string name, double radius)
+        {
+            Side = radius;
+            Name = name;
+        }
+        public double GetArea()
+        {
+            return Side * Side;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+}
