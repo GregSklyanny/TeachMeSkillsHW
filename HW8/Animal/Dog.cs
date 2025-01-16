82e028740fdff9998a8da77e99f4032688141996
GregSklyanny <gregsklyanny@gmail.com>
2025-01-16 16:02:26 +0300

    HW8 Animal

---
+++ HW8/Animal/Dog.cs
@@ -0,0 +1,28 @@
+﻿using Animal.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    public class Dog : AnimalBase
+    {
+        public string Name { get; private set; } = "Без имени";
+        public override void Eat()
+        {
+            Console.WriteLine($"собака по имени {Name} ест");
+        }
+
+        public override string GetName()
+        {
+            return Name;
+        }
+
+        public override void SetName(string name)
+        {
+            Name = name;
+        }
+    }
+}
