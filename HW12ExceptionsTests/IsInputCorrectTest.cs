3d6390780680566d42044a6d5997817ca5876735
GregSklyanny <gregsklyanny@gmail.com>
2025-01-20 12:48:11 +0300

    HW12TestsAdded

--- HW12ExceptionsTests/IsInputCorrectTest.cs
+++ HW12ExceptionsTests/IsInputCorrectTest.cs
@@ -22,5 +22,25 @@ namespace HW12ExceptionsTests
             Assert.False(result);
         }
 
+        [Theory]
+        [InlineData("")]
+        [InlineData("Greg")]
+        [InlineData("adsdqwdqwdqwdqwgwrg545g5ggewgwegwegwerwerwerw3r32r23r23r23r")]
+        [InlineData("Gre G")]
+        public void CheckLoginValidation(string login)
+        {
+            Assert.Throws<WrongLoginException>(() => InputValidator.ValidateLogin(login));
+        }
+
+        [Theory]
+        [InlineData("123","123")]
+        [InlineData("1 23", "1 23")]
+        [InlineData("12asdasdasdasdasdasdasdqwdqwdqwdqwfgqwfqwfqwfqwfqwf3", "12asdasdasdasdasdasdasdqwdqwdqwdqwfgqwfqwfqwfqwfqwf3")]
+        [InlineData("afafssaf", "afafssaf")]
+        public void CheckPasswordValidation(string password, string passwordConfirmation)
+        {
+            Assert.Throws<WrongPasswordException>(() => InputValidator.ValidatePassword(password, passwordConfirmation));
+        }
+
     }
 }
