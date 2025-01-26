715fb11440d226e78ed4dbeba1dcea7147f1a66c
GregSklyanny <gregsklyanny@gmail.com>
2025-01-26 14:50:33 +0300

    HW10Rework

---
+++ HW10/DelegatesProgram/ExecutionManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesProgram
+{
+    public class ExecutionManager
+    {
+        private OperationManager _operationManager;
+        public Dictionary<Operation, Func<int>> FuncExecute { get; set; }
+        public ExecutionManager(OperationManager operationManager)
+        {
+            _operationManager = operationManager;
+            FuncExecute = new Dictionary<Operation, Func<int>>();
+        }
+        public void PopulateFunctions(Operation operation, Func<int> func)
+        {
+            FuncExecute.Add(operation, func);
+        }
+        public void PrepareExecution()
+        {
+            PopulateFunctions(Operation.Sum, _operationManager.Execute(Operation.Sum));
+            PopulateFunctions(Operation.Subtract, _operationManager.Execute(Operation.Subtract));
+            PopulateFunctions(Operation.Multiply, _operationManager.Execute(Operation.Multiply));
+        }
+    }
+}
---
+++ HW10/DelegatesProgram/OperationManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesProgram
+{
+    public enum Operation
+    {
+        Sum,
+        Subtract,
+        Multiply
+    }
+    public class OperationManager
+    {
+        private int _first;
+        private int _second;
+        public ExecutionManager executionManager;
+        public OperationManager(int first, int second)
+        {
+            _first = first;
+            _second = second;
+            executionManager = new ExecutionManager(this);
+            executionManager.PrepareExecution();
+        }
+        private int Sum()
+        {
+            return _first + _second;
+        }
+        private int Subtract()
+        {
+            return _first - _second;
+        }
+        private int Multiply()
+        {
+            return _first * _second;
+        }
+        public Func<int> Execute(Operation operation)
+        {
+            switch (operation)
+            {
+                case Operation.Sum:
+                    return Sum;
+                case Operation.Subtract:
+                    return Subtract;
+                case Operation.Multiply:
+                    return Multiply;
+                default:
+                    return null;
+            }
+        }
+    }
+}
