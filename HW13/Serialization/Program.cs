3ee215bfd5246c1e339c36a862df2e04b16db923
GregSklyanny <gregsklyanny@gmail.com>
2025-01-28 22:20:53 +0300

    HW13

---
+++ HW13/Serialization/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputDirectory = Directory.GetCurrentDirectory() + "/inputdata";
+            var jsonFiles = Directory.GetFiles(inputDirectory, "*.json");
+            Squad squad = null;
+            foreach (string file in jsonFiles)
+            {
+                try
+                {
+                using (FileStream fs = new FileStream(jsonFiles[0], FileMode.Open))
+                {
+                    squad = JsonSerializer.Deserialize<Squad>(fs);
+                }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+                try
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(Squad));
+                    string outputXMLFile = inputDirectory + $"/{squad.SquadName}.xml";
+                    using (FileStream fs = new FileStream(outputXMLFile, FileMode.OpenOrCreate))
+                    {
+                        xmlSerializer.Serialize(fs, squad);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }               
+        }
+    }
+}
