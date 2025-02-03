5fc99d994682ee17ed4b289a4629971ba7d4e863
GregSklyanny <gregsklyanny@gmail.com>
2025-02-03 16:44:51 +0300

    HW14init

---
+++ HW14/HTTPMethods/HTTPMethods.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTPMethods
+{
+    public class HTTPMethods
+    {
+        public static async Task<string> Get(string url)
+        {
+            string content;
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                content = await response.Content.ReadAsStringAsync();
+            }
+            return content;
+        }
+        public static async Task<string> Post(string url, HttpContent contentToPost)
+        {
+            string content;
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(url, contentToPost);
+                content = await response.Content.ReadAsStringAsync();
+            }
+            return content;
+        }
+        public static async Task<string> Put(string url, HttpContent contentToPost)
+        {
+            string content;
+            using (var client = new HttpClient())
+            {
+                var response = await client.PutAsync(url, contentToPost);
+                content = await response.Content.ReadAsStringAsync();
+            }
+            return content;
+        }
+        public static async Task<string> Patch(string url, HttpContent contentToPost)
+        {
+            string content;
+            using (var client = new HttpClient())
+            {
+                var response = await client.PatchAsync(url, contentToPost);
+                content = await response.Content.ReadAsStringAsync();
+            }
+            return content;
+        }
+        public static async Task<string> Delete(string url)
+        {
+            string content;
+            using (var client = new HttpClient())
+            {
+                var response = await client.DeleteAsync(url);
+                content = await response.Content.ReadAsStringAsync();
+            }
+            return content;
+        }
+    }
+}
---
+++ HW14/HTTPMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Sockets;
+
+namespace HTTPMethods
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var content = new StringContent("This is my content");
+            
+            Console.WriteLine(await HTTPMethods.Get("https://httpbin.org/"));
+            Console.WriteLine(await HTTPMethods.Post("https://httpbin.org/post", content));
+            Console.WriteLine(await HTTPMethods.Put("https://httpbin.org/put", content));
+            Console.WriteLine(await HTTPMethods.Patch("https://httpbin.org/patch", content));
+            Console.WriteLine(await HTTPMethods.Delete("https://httpbin.org/delete"));
+        }
+    }
+}
