f8164e476360297effc9c43cb90963d6a50e226d
GregSklyanny <gregsklyanny@gmail.com>
2025-02-03 16:50:45 +0300

    Update HTTPMethods.cs

--- HW14/HTTPMethods/HTTPMethods.cs
+++ HW14/HTTPMethods/HTTPMethods.cs
@@ -10,52 +10,37 @@ namespace HTTPMethods
     {
         public static async Task<string> Get(string url)
         {
-            string content;
-            using (var client = new HttpClient())
-            {
-                var response = await client.GetAsync(url);
-                content = await response.Content.ReadAsStringAsync();
-            }
+            using var client = new HttpClient() ;
+            var response = await client.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
             return content;
         }
         public static async Task<string> Post(string url, HttpContent contentToPost)
         {
-            string content;
-            using (var client = new HttpClient())
-            {
-                var response = await client.PostAsync(url, contentToPost);
-                content = await response.Content.ReadAsStringAsync();
-            }
+            using var client = new HttpClient();
+            var response = await client.PostAsync(url, contentToPost);
+            var content = await response.Content.ReadAsStringAsync();
             return content;
         }
         public static async Task<string> Put(string url, HttpContent contentToPost)
         {
-            string content;
-            using (var client = new HttpClient())
-            {
-                var response = await client.PutAsync(url, contentToPost);
-                content = await response.Content.ReadAsStringAsync();
-            }
+            using var client = new HttpClient();
+            var response = await client.PutAsync(url, contentToPost);
+            var content = await response.Content.ReadAsStringAsync();
             return content;
         }
         public static async Task<string> Patch(string url, HttpContent contentToPost)
         {
-            string content;
-            using (var client = new HttpClient())
-            {
-                var response = await client.PatchAsync(url, contentToPost);
-                content = await response.Content.ReadAsStringAsync();
-            }
+            using var client = new HttpClient();
+            var response = await client.PatchAsync(url, contentToPost);
+            var content = await response.Content.ReadAsStringAsync();
             return content;
         }
         public static async Task<string> Delete(string url)
         {
-            string content;
-            using (var client = new HttpClient())
-            {
-                var response = await client.DeleteAsync(url);
-                content = await response.Content.ReadAsStringAsync();
-            }
+            using var client = new HttpClient();
+            var response = await client.DeleteAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
             return content;
         }
     }
